e9abedbcd34799b3274e123b362d7f5555016128
Divyanshu Gupta <divyanshu4760@gmail.com>
2026-09-15 22:24:04 +0530

    test(identity): make AZURE_AUTHORITY_HOST env mutation exception-safe
    The clientSecretCredential tests mutated the process-global
    AZURE_AUTHORITY_HOST with bare setEnv/unsetEnv, so a thrown assertion
    would skip the unsetEnv and leak the stubbed host into later specs
    (resolveAuthorityHost is reused by Tasks 3/6/7). Wrap the mutation in
    a bracket_-based withEnvVar helper so it's always restored.

--- test/IdentitySpec.hs
+++ test/IdentitySpec.hs
@@ -3,6 +3,7 @@ module IdentitySpec (spec) where
 import Azure.Core.Credential (AccessToken (..), Credential (..), Scope (..), TokenSource (..))
 import Azure.Core.Error (AzureError (..))
 import Azure.Identity
+import Control.Exception (bracket_)
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy.Char8 as LC
 import qualified Data.Text as T
@@ -29,34 +30,35 @@ spec = describe "Azure.Identity" $ do
     it "POSTs client_credentials and parses the access token" $ do
       mgr <- newTlsManager
       let body = "{\"token_type\":\"Bearer\",\"expires_in\":3599,\"access_token\":\"abc.def\"}"
-      withStub [(status200, [], body)] $ \baseUrl reqs -> do
-        setEnv "AZURE_AUTHORITY_HOST" (T.unpack baseUrl)
-        src <- case clientSecretCredential (TenantId "t1") (ClientId "c1") (mkClientSecret "s3cr3t") of
-          Entra s -> pure s
-          _ -> error "clientSecretCredential did not return an Entra credential"
-        tok <- tsFetch src mgr (Scope "https://storage.azure.com/.default")
-        atToken tok `shouldBe` "abc.def"
-        [r] <- reqs
-        recMethod r `shouldBe` "POST"
-        recPath r `shouldBe` "/t1/oauth2/v2.0/token"
-        let form = BC.unpack (LC.toStrict (recBody r))
-        form `shouldContain` "grant_type=client_credentials"
-        form `shouldContain` "client_id=c1"
-        form `shouldContain` "scope=https%3A%2F%2Fstorage.azure.com%2F.default"
-        form `shouldContain` "client_secret=s3cr3t"
-        unsetEnv "AZURE_AUTHORITY_HOST"
+      withStub [(status200, [], body)] $ \baseUrl reqs ->
+        withEnvVar "AZURE_AUTHORITY_HOST" (T.unpack baseUrl) $ do
+          src <- case clientSecretCredential (TenantId "t1") (ClientId "c1") (mkClientSecret "s3cr3t") of
+            Entra s -> pure s
+            _ -> error "clientSecretCredential did not return an Entra credential"
+          tok <- tsFetch src mgr (Scope "https://storage.azure.com/.default")
+          atToken tok `shouldBe` "abc.def"
+          [r] <- reqs
+          recMethod r `shouldBe` "POST"
+          recPath r `shouldBe` "/t1/oauth2/v2.0/token"
+          let form = BC.unpack (LC.toStrict (recBody r))
+          form `shouldContain` "grant_type=client_credentials"
+          form `shouldContain` "client_id=c1"
+          form `shouldContain` "scope=https%3A%2F%2Fstorage.azure.com%2F.default"
+          form `shouldContain` "client_secret=s3cr3t"
 
     it "maps a non-2xx body to a ServiceError" $ do
       mgr <- newTlsManager
       let body = "{\"error\":\"invalid_client\",\"error_description\":\"bad secret\"}"
-      withStub [(status401, [], body)] $ \baseUrl _ -> do
-        setEnv "AZURE_AUTHORITY_HOST" (T.unpack baseUrl)
-        src <- case clientSecretCredential (TenantId "t1") (ClientId "c1") (mkClientSecret "nope") of
-          Entra s -> pure s
-          _ -> error "clientSecretCredential did not return an Entra credential"
-        tsFetch src mgr (Scope "https://storage.azure.com/.default")
-          `shouldThrow` \e -> case e of ServiceError {} -> True; _ -> False
-        unsetEnv "AZURE_AUTHORITY_HOST"
+      withStub [(status401, [], body)] $ \baseUrl _ ->
+        withEnvVar "AZURE_AUTHORITY_HOST" (T.unpack baseUrl) $ do
+          src <- case clientSecretCredential (TenantId "t1") (ClientId "c1") (mkClientSecret "nope") of
+            Entra s -> pure s
+            _ -> error "clientSecretCredential did not return an Entra credential"
+          tsFetch src mgr (Scope "https://storage.azure.com/.default")
+            `shouldThrow` \e -> case e of ServiceError {} -> True; _ -> False
   where
     isLeft = either (const True) (const False)
     summarise = either (("Left " <>) . show) (const "Right <credential>")
+
+    withEnvVar :: String -> String -> IO a -> IO a
+    withEnvVar k v = bracket_ (setEnv k v) (unsetEnv k)
